8e9939d2e39ff8cda833c7480038348c89d2d7c5
harutaka <10650294+harutaka@users.noreply.github.com>
2021-06-24 13:08:26 +0900

    fix underbar start file name

---
+++ docs/nextdir/static/WgSFhdFz3LB-xNLqOxefS/buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/453-a9c6d935bfeb339cc4a9.js","static/chunks/pages/index-fe7fb64e97de866c0938.js"],"/_error":["static/chunks/pages/error-157366ffea25803f02fd.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
